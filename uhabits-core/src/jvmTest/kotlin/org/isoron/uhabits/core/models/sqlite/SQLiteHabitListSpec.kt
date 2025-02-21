4c05ac172d542ffbb2d9211b54c1895f72aba3dc
Adel <shamsutoff@yandex.ru>
2025-02-21 14:09:56 +0300

    SQL Test. First part

--- uhabits-core/src/jvmTest/kotlin/org/isoron/uhabits/core/models/sqlite/SQLiteHabitListSpec.kt
+++ uhabits-core/src/jvmTest/kotlin/org/isoron/uhabits/core/models/sqlite/SQLiteHabitListSpec.kt
@@ -2,9 +2,12 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.spyk
+import io.mockk.verify
 import org.isoron.uhabits.core.database.Repository
 import org.isoron.uhabits.core.models.Habit
 import org.isoron.uhabits.core.models.ModelFactory
+import org.isoron.uhabits.core.models.ModelObservable
 import org.isoron.uhabits.core.models.sqlite.SQLiteEntryList
 import org.isoron.uhabits.core.models.sqlite.SQLiteHabitList
 import org.isoron.uhabits.core.models.sqlite.records.HabitRecord
@@ -16,10 +19,12 @@ class SQLiteHabitListSpec : StringSpec({
     "should add, get and delete habit" {
         val modelFactory = mockk<ModelFactory>(relaxed = true)
         val repository = mockk<Repository<HabitRecord>>(relaxed = true)
+        val mockObservable = mockk<ModelObservable>(relaxed = true)
 
         every { modelFactory.buildHabitListRepository() } returns repository
 
-        val habitList = SQLiteHabitList(modelFactory)
+        val habitList = spyk(SQLiteHabitList(modelFactory))
+        every { habitList.observable } returns mockObservable
 
         val habit = Habit(
             id = Random.nextLong(),
@@ -36,7 +41,8 @@ class SQLiteHabitListSpec : StringSpec({
 
         habit.id?.let { habitId ->
             every { repository.save(any<HabitRecord>()) } answers {
-                firstArg<HabitRecord>().id = habitId
+                val record = firstArg<HabitRecord>()
+                record.id = habitId
             }
 
             every { repository.find(habitId) } returns habitRecord
@@ -44,15 +50,19 @@ class SQLiteHabitListSpec : StringSpec({
             habitList.size() shouldBe 0
 
             habitList.add(habit)
+            verify { repository.save(any<HabitRecord>()) }
+            verify { mockObservable.notifyListeners() }
 
             habitList.size() shouldBe 1
             habitList.getById(habitId) shouldBe habit
 
             habitList.remove(habit)
 
+//            verify { repository.remove(any<HabitRecord>()) }
+            verify { mockObservable.notifyListeners() }
+
             habitList.getById(habitId) shouldBe null
             habitList.size() shouldBe 0
         }
     }
-
 })
